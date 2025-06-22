703b4fa4acdf9dc38a169f2446914a7d53fb405b
karat <karateev.92@gmail.com>
2025-06-22 13:05:46 +0400

    добавлены представления и шаблоны для управления клиентами

---
+++ mailings/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'mailings'
+
+urlpatterns = [
+    path('clients/', views.ClientListView.as_view(), name='client_list'),
+    path('clients/<int:pk>/', views.ClientDetailView.as_view(), name='client_detail'),
+    path('clients/create/', views.ClientCreateView.as_view(), name='client_create'),
+    path('clients/<int:pk>/update/', views.ClientUpdateView.as_view(), name='client_update'),
+    path('clients/<int:pk>/delete/', views.ClientDeleteView.as_view(), name='client_delete'),
+]
--- mailings/views.py
+++ mailings/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Client
 
-# Create your views here.
+class ClientListView(ListView):
+    model = Client
+    template_name = 'mailings/client_list.html'
+    context_object_name = 'clients'
+
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = 'mailings/client_detail.html'
+    context_object_name = 'client'
+
+class ClientCreateView(CreateView):
+    model = Client
+    template_name = 'mailings/client_form.html'
+    fields = ['email', 'full_name', 'comment']
+    success_url = reverse_lazy('mailings:client_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    template_name = 'mailings/client_form.html'
+    fields = ['email', 'full_name', 'comment']
+    success_url = reverse_lazy('mailings:client_list')
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = 'mailings/client_confirm_delete.html'
+    success_url = reverse_lazy('mailings:client_list')
