30fe551d3e78d3b2b96a419058a84b04c04c9591
karat <karateev.92@gmail.com>
2025-06-22 18:21:36 +0400

    Реализована регистрация пользователя: форма, представление, шаблон. Подключён кастомный пользователь, подключена база PostgreSQL, настроена русская локализация и шаблоны на Bootstrap

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -103,19 +104,29 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [BASE_DIR / 'locale']
+
+
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Каталог, где лежат твои CSS/JS в dev-режиме
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Каталог, куда collectstatic всё сложит для продакшна
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import RegisterForm
 
-# Create your views here.
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('mailings:home')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
