829f6d627ee13f95c28300a9f460ec0bfd0a4aa9
karat <karateev.92@gmail.com>
2025-06-22 12:24:24 +0400

    добавлена модель CustomUser, Pillow, миграции применены

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']  # Обязательное поле при переопределении логина
+
+    def __str__(self):
+        return self.email
