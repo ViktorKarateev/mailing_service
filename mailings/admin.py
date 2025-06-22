e61fc8daf433d41c799471175cddbf7b2eb374c5
karat <karateev.92@gmail.com>
2025-06-22 12:51:13 +0400

    зарегистрированы модели в админке, создан суперпользователь

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Client, Message, Mailing, MailingLog
 
-# Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name', 'owner')
+    search_fields = ('email', 'full_name')
+    list_filter = ('owner',)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'owner')
+    search_fields = ('subject',)
+    list_filter = ('owner',)
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('start_time', 'end_time', 'status', 'owner')
+    list_filter = ('status', 'owner')
+
+@admin.register(MailingLog)
+class MailingLogAdmin(admin.ModelAdmin):
+    list_display = ('attempt_time', 'status', 'mailing')
+    list_filter = ('status', 'mailing')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'phone', 'country', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('avatar', 'phone', 'country')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('avatar', 'phone', 'country')}),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
