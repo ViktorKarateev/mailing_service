8fbdd11e7ecc370efc3f606051c726e2ccd7d256
karat <karateev.92@gmail.com>
2025-06-22 12:29:31 +0400

    добавлены модели Client, Message, Mailing, MailingLog и миграции

--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Client(models.Model):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    comment = models.TextField(blank=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='clients'
+    )
+
+    def __str__(self):
+        return f"{self.full_name} <{self.email}>"
+
+
+class Message(models.Model):
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
+
+    def __str__(self):
+        return self.subject
+
+class Mailing(models.Model):
+    STATUS_CHOICES = [
+        ('created', 'Создана'),
+        ('launched', 'Запущена'),
+        ('completed', 'Завершена'),
+    ]
+
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='created')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    clients = models.ManyToManyField(Client)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='mailings'
+    )
+
+    def __str__(self):
+        return f"Рассылка от {self.start_time} до {self.end_time}"
+
+
+class MailingLog(models.Model):
+    STATUS_CHOICES = [
+        ('success', 'Успешно'),
+        ('failed', 'Не успешно'),
+    ]
+
+    attempt_time = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    server_response = models.TextField()
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"[{self.get_status_display()}] {self.attempt_time}"
